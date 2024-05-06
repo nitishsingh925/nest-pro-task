0134b00d609765ec86a965f990b38895b6077a47
nitish singh <nitishsingh925@gmail.com>
2024-05-06 13:24:10 +0530

    user data validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(20)
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  @IsNotEmpty()
+  name: string;
+}
